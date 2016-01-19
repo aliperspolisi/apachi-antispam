449938ed77ff54d0b44168316610469e675a9035
ali perspolisi <ali.salehi1379@yahoo.com>
2016-01-19 15:42:52 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {omidtarh,70480064},
+    sudo_users = {bokone_kise,77562275},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
